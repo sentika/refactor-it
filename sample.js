bd877062e6672b6c855b8c6d40052d1c341fec8c
Alex Mesch <mesheryakov.alexander@mail.ru>
2019-11-07 20:37:13 +0300

    Refactored sample

--- sample.js
+++ sample.js
@@ -3,86 +3,87 @@
  Выводим результаты в консоль.
  Запуск через node sample.js
  */
-var players = [
+'use strict';
+
+let players = [
     'Alexey',
     'Artur',
     'Kirill',
     'Vlad'
-]
-
-'using strict';
+];
 
 class Game {
-    constructor({p1, n2}) {
-        this.player1 = p1;
-        this.player2 = n2;
-
-        this.isPlayer1Win = undefined;
+    constructor({ player1, player2 }) {
+        this.p1 = player1;
+        this.p2 = player2;
     }
 
-play() {
-    var player1Win = Math.round(Math.random());
-    switch (player1Win) {
-        case 1:
-            this.isPlayer1Win = true;
-            console.info(this.player1, '1 - 0', this.player2);
-        case 0:
-            if (!this.isPlayer1Win) {
-                console.info(this.player1, '0 - 1', this.player2);
-            }
+    displayResult() {
+        console.info(this.p1, `${oneZero} - ${1 - oneZero}`, this.p2);
     }
-}}
+
+    playGame() {
+        this.player1Win = Math.round(Math.random());
+        this.displayResult(this.player1Win);
+    }
+}
 
 class Championship {
-    constructor(gamerzz) {
-        this.games = []
-        this._t = gamerzz;
-        this.setSchedule()
+    constructor(playersList) {
+        this.gamesList = []
+        this.firstRoundGamesList = [];
+        this.secondRoundGamesList = [];
+
+        this._playersList = playersList;
+
+        this.setGamesList(this.firstRoundGamesList);
+        this.setGamesList(this.secondRoundGamesList);
     }
 
-    setSchedule() {
-        for (var i = 0; i < this._t.length; i++) {
-            for (var j = 0; j < this._t.length; j++) {
-                if (i < j) this.games.push(new Game({p1: this._t[i], n2: this._t[j]}));
-            }
-        }
-
-        // должен быть второй круг с надписью второй круг перед его стартом, но почему-то не работает :(
-        // console.info('2nd round');
-
-        // rematches // 2nd round
-        for (var i = 0; i < this._t.length; i++) {
-            for (var j = 0; j < this._t.length; j++) {
-                if (i < j) this.games.push(new Game({p1: this._t[i], n2: this._t[j]}));
+    setGamesList(gamesList) {
+        for (let i = 0; i < this._playersList.length; i++) {
+            for (let j = 0; j < this._playersList.length; j++) {
+                if (i < j) {
+                    gamesList.push(new Game({
+                        player1: this._listOfPlayers[i],
+                        player2: this._listOfPlayers[j]
+                    }));
+                }
             }
         }
     }
 
-    play() {
-        this.games.map((game) => game.play());
+    //1st round
+    beginFirstRound() {
+        console.info("Round 1!");
+        this.firstRoundGamesList.map((game) => game.playGame());
+    }
+
+    //2nd round
+    beginSecondRound() {
+        console.info("Round 2!");
+        this.secondRoundGamesList.map((game) => game.playGame());
+    }
+
+    startChamp() {
+        this.beginFirstRound();
+        this.beginSecondRound();
+        this.gamesList = this.firstRoundGamesList.concat(this.secondRoundGamesList);
     }
 
     getResults() {
-        return this.games.reduce((prev, current) => {
-            if (current.isPlayer1Win) {
-                return {
-                    ...prev,
-                    [current.player1]: (prev[current.player1] || 0) + 1,
-                    [current.player2]: (prev[current.player2] || 0)
-                }
-            }
-
+        return this.gamesList.reduce((previous, current) => {
             return {
-                ...prev,
-                [current.player1]: (prev[current.player1] || 0),
-                [current.player2]: (prev[current.player2] || 0) + 1
+                ...previous,
+                [current.player1]: (previous[current.player1] || 0) + current.player1Win,
+                [current.player2]: (previous[current.player2] || 0) + 1 - current.player1Win
             }
         }, {})
     }
 }
 
 
-const champinonship = new Championship(players);
-champinonship.play();
-let results = champinonship.getResults();
+const championship = new Championship(players);
+championship.startChamp();
+let results = championship.getResults();
 console.info('Results', results);
